8a63d803a352aa3d9cdbb5662f059d91cc411571
alextheprophecy <91223807+alextheprophecy@users.noreply.github.com>
2024-07-16 10:01:39 +0200

    basic page

---
+++ src/components/animal/animal.component.js
@@ -0,0 +1,8 @@
+import '../../styles/animal/animal.css'
+const Animal = () => {
+    return (
+        <div className="main-container">
+        </div>
+    )
+}
+export default Animal;
---
+++ src/components/main/main.component.js
@@ -0,0 +1,8 @@
+import '../../styles/main/main.css'
+const MainComponent = () => {
+    return (
+        <div className="main-container">
+        </div>
+    )
+}
+export default MainComponent;
