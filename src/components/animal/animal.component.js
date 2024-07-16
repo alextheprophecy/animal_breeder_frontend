ae668b903170f68336c066270818c354da90ad10
alextheprophecy <91223807+alextheprophecy@users.noreply.github.com>
2024-07-17 00:33:56 +0200

    feat: created basic components, added random moving animal sprites

--- src/components/animal/animal.component.js
+++ src/components/animal/animal.component.js
@@ -1,8 +1,37 @@
 import '../../styles/animal/animal.css'
-const Animal = () => {
+import {useEffect, useState} from "react";
+import useWindowDimensions from "../../hooks/useWindowDimensions";
+
+const Animal = ({id, image}) => {
+    const WINDOW = useWindowDimensions() //TODO: fetch values from parent
+    const BOUNDS = {width: WINDOW.width - (300/2), height: WINDOW.height - 150} //TODO: care that the offset are hardcoded
+
+    let [pos, setPos] = useState(randomPos())
+    function randomPos() {return {x: Math.random() * BOUNDS.width, y: Math.random() * BOUNDS.height}}
+    function randomBetween(min, max) {return min + Math.random()*(max-min)}
+    function moveRandom() {setPos(randomPos())}
+    const CallInterval = (minSeconds, maxSeconds, callback) => {
+        useEffect(() => {
+            let timer
+            const refresh = () => {
+                timer = setTimeout(() => {
+                    callback()
+                    refresh()
+                }, randomBetween(minSeconds, maxSeconds) * 1000)
+            }
+            refresh()
+            return () => clearTimeout(timer)
+        }, [])
+    }
+
+    CallInterval(1, 7, ()=> moveRandom())
+
     return (
-        <div className="main-container">
+        <div className="animal-container" style={{left: `${pos.x}px`, top: `${pos.y}px`}}>
+            <img src={image} style={{width: '100%'}}/>
         </div>
     )
 }
+
+
 export default Animal;
--- src/components/main/main.component.js
+++ src/components/main/main.component.js
@@ -1,7 +1,13 @@
 import '../../styles/main/main.css'
+import Animal from "../animal/animal.component";
+
 const MainComponent = () => {
+
     return (
         <div className="main-container">
+            <Animal image={'pictures/bear_spider.jpg'}/>
+            <Animal image={'pictures/frogHead_caterpillar.webp'}/>
+            <Animal image={'pictures/eagleHead_mosquito.webp'}/>
         </div>
     )
 }
